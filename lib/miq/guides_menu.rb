2962efc0e0a4ffe01de545f7556c6c845cfaec47
Nick LaMuro <nicklamuro@gmail.com>
2020-02-26 20:23:51 -0600

    [POC] performance for doc menu

--- lib/miq/guides_menu.rb
+++ lib/miq/guides_menu.rb
@@ -6,6 +6,10 @@ module Miq
       new.build
     end
 
+    def self.build_doc_menu_html
+      new.build_doc_menu_html
+    end
+
     attr_reader :source_dir, :output_dir
 
     def initialize(transform_paths: true)
@@ -30,7 +34,39 @@ module Miq
       end
     end
 
+    MENU_TEMPLATE = <<~MENU_TEMPLATE
+      {% assign hpath = page.url | append: ".html" %}
+      {% assign rpath = page.url | remove: "/index" %}
+
+      <ul class="menu menu-toplevel" id="guides_menu">
+      <% parent_items.each do |group| -%>
+        <li class="menu-parent {% if "<%= group["path"] %>" == page.url or "<%= group["path"] %>" == hpath %} active menu-open{% endif %}">
+          <%= render_menu_link_for group %>
+          <% if group["children"] && group["children"].size > 0 %>
+          <%= render_children group.children %>
+          <% end -%>
+        </li>
+      <% end -%>
+      <% solo_items.each do |group| -%>
+        <li{% if "<%= group["path"] %>" == page.url or "<%= group["path"] %>" == hpath %} class="active menu-open"{% endif %}>
+          <%= render_menu_link_for group %>
+        </li>
+      <% end -%>
+      </ul>
+    MENU_TEMPLATE
+
+    # TODO:  Either combine with `#build`, or delete the code for that
+    def build_doc_menu_html
+      menu_items   = site.data["menus"]["guides_menu"]["children"]
+      parent_items = menu_items.select {|e| e["children"] }
+      solo_items   = menu_items.reject {|e| e["children"] }
+    end
+
     private
+
+    def render_menu_link_for item
+      %(<a href="#{item["path"] || "#"}">#{group["title"]}</a>)
+    end
 
     def walk_tree(path, title = nil)
       title = title || File.basename(path)
